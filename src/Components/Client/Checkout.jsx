3dd17079cbda5d18c840812dec07f329917982e3
Mohamed Shareef <m.shareef.km@gmail.com>
2023-09-21 14:41:13 +0530

    cart and checkout

---
+++ src/Components/Client/Checkout.jsx
@@ -0,0 +1,235 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+import UserAxios from "../../Axios/UserAxios";
+import AddressModal from "../../assets/AddressModal";
+import Button from "../../assets/Button";
+
+function Checkout() {
+  const navigate = useNavigate();
+
+  const [selectedAddressIndex, setSelectedAddressIndex] = useState(null);
+  const [payment, setPayment] = useState("COD");
+  const [is_change, set_change] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [cartData, setCartData] = useState();
+  const [address, setAddress] = useState([
+    {
+      street: "",
+      city: "",
+      state: "",
+      postalCode: "",
+    },
+  ]);
+  const [newAddress, setNewAddress] = useState({
+    street: "",
+    city: "",
+    state: "",
+    postalCode: "",
+  });
+
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    UserAxios.get(`/profile?id=${user._id}`).then((response) => {
+      //   setUserData(response);
+      setAddress(response?.data?.user?.Address);
+    });
+  }, [is_change]);
+
+  useEffect(() => {
+    UserAxios.get(`/getcart?id=${user._id}`).then((response) => {
+      setCartData(response.data.cartData);
+    });
+  }, []);
+
+  const handleSaveAddress = (userId) => {
+    if (
+      newAddress.street.trim().length === 0 ||
+      newAddress.city.trim().length === 0 ||
+      newAddress.state.trim().length === 0 ||
+      newAddress.postalCode.trim().length === 0
+    ) {
+      toast.error("Please fill all field", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000,
+      });
+    } else {
+      UserAxios.patch("/addaddress", { id: userId, address: newAddress })
+        .then((response) => {
+          if (response) {
+            set_change(!is_change);
+            toast.success(response.data.message, {
+              position: toast.POSITION.TOP_CENTER,
+              autoClose: 3000,
+            });
+          } else {
+            toast.error(response.data.message, {
+              position: toast.POSITION.TOP_CENTER,
+              autoClose: 3000,
+            });
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          toast.error(error.response.data.message, {
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 3000,
+          });
+        });
+    }
+
+    setIsModalOpen(false);
+  };
+
+  const handleRadioChange = (index) => {
+    setSelectedAddressIndex(index);
+  };
+
+  const handlePaymentRadio = (payMethod) => {
+    setPayment(payMethod);
+  };
+
+  const placeOrder = (payment) => {
+    if (selectedAddressIndex == null) {
+      console.log(selectedAddressIndex);
+      toast.error("Please select address", {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 3000,
+      });
+    } else {
+      UserAxios.post("/order", {
+        payment,
+        addressIndex: selectedAddressIndex,
+        cartData,
+      }).then((response) => {
+        toast.success(response.data.message, {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 3000,
+        });
+        navigate("/orders");
+      }).catch((err)=>{
+        toast.error(err.response.data.message, {
+            position: toast.POSITION.TOP_CENTER,
+            autoClose: 3000,
+          });
+      })
+    }
+  };
+
+  return (
+    <div className="lg:ml-16 md:mr-16 mb-7 mt-7">
+      <div className="lg:flex w-full md:p-5">
+        <div className="lg:w-2/3 lg:ml-2 h-screan">
+          <div className="w-full lg:p-10 leading-loose">
+            <label className="block text-sm font-medium text-gray-700 underline">
+              Select Address :
+            </label>
+            <div className="flex items-center justify-around">
+              {address?.map((elem, index) => (
+                <div key={index}>
+                  <input
+                    type="radio"
+                    name="selectedAddress"
+                    value={index}
+                    checked={selectedAddressIndex === index}
+                    onChange={() => handleRadioChange(index)}
+                  />
+                  <p>Street: {elem?.street}</p>
+                  <p>City: {elem?.city}</p>
+                  <p>State: {elem?.state}</p>
+                  <p>Postal Code: {elem?.postalCode}</p>
+                </div>
+              ))}
+            </div>
+
+            <button
+              className="p-4 text-green-600"
+              onClick={() => {
+                setIsModalOpen(true);
+              }}
+            >
+              Add Address
+            </button>
+          </div>
+
+          <AddressModal
+            isOpen={isModalOpen}
+            onClose={() => setIsModalOpen(false)}
+            address={newAddress}
+            onSave={handleSaveAddress}
+            setNewAddress={setNewAddress}
+          />
+        </div>
+
+        <div className="lg:w-1/3 h-screen">
+          <div className="h-2/3 border w-full bg-pink-100 lg:p-10">
+            <div className="h-full  shadow-md ">
+              <div className="space-y-4 p-4">
+                <h1>
+                  Total: <span className="float-right">{cartData?.total}</span>
+                </h1>
+                <h1>
+                  Charges:<span className="float-right">{0}</span>
+                </h1>
+                <h1>
+                  Discount:{" "}
+                  <span className="float-right">{cartData?.discount}</span>
+                </h1>
+                <h1>
+                  Grand Total:{" "}
+                  <span className="float-right">{cartData?.grandTotal}</span>
+                </h1>
+              </div>
+              <div>
+                <div className="flex justify-between pl-3 pr-3 pt-14">
+                  <div className="">
+                    <input
+                      type="radio"
+                      name="selectPayment"
+                      value={"COD"}
+                      checked={payment === "COD"}
+                      onChange={() => handlePaymentRadio("COD")}
+                    />
+                    <label className="ml-1">COD</label>
+                  </div>
+                  <div>
+                    <input
+                      type="radio"
+                      name="selectPayment"
+                      value={"Online"}
+                      checked={payment === "Online"}
+                      onChange={() => handlePaymentRadio("Online")}
+                    />
+                    <label className="ml-1">Online</label>
+                  </div>
+                  <div>
+                    <input
+                      type="radio"
+                      name="selectPayment"
+                      value={"Wallet"}
+                      checked={payment === "Wallet"}
+                      onChange={() => handlePaymentRadio("Wallet")}
+                    />
+                    <label className="ml-1">Wallet</label>
+                  </div>
+                </div>
+                <Button
+                  value={"Place Order"}
+                  className={"mt-7 w-full"}
+                  onClick={() => {
+                    placeOrder(payment);
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
---
+++ src/Components/Client/Orders.jsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import Swal from "sweetalert2";
+
+import UserAxios from "../../Axios/UserAxios";
+
+function Orders() {
+  let total = 0;
+  let charges = 0;
+  let discount = 0;
+  let grandTotal = 0;
+  const [orderItem, setOrderItem] = useState([]);
+  const [cartId, setCartId] = useState({});
+  const [is_chage, setChange] = useState(false);
+
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    UserAxios.get(`/getorders?id=${user._id}`).then((response) => {
+      const items = response.data.orders;
+      setOrderItem(items);
+      // setCartId(response.data.cartData);
+    });
+  }, []);
+
+  const cancelCartItem = async (id) => {
+    const result = await Swal.fire({
+      title: "Do you really want to delete this product?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel",
+    });
+    if (result.isConfirmed) {
+      UserAxios.patch("/cancelcartitem", {
+        itemId: id,
+        cartId: cartId._id,
+      }).then((response) => {
+        setChange(!is_chage);
+        toast.success(response.data.message, {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 3000,
+        });
+      });
+    }
+  };
+
+  return (
+    <div className="p-10">
+      <div className="border md:flex">
+        <div className="h-full md:w-2/3">
+          <div className="w-full overflow-x-auto">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className=" bg-table-blue text-off-White">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium  uppercase tracking-wider">
+                    PRODUCT
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium  uppercase tracking-wider">
+                    QUANTITY
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium  uppercase tracking-wider">
+                    RATE
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium  uppercase tracking-wider">
+                    PRICE
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium  uppercase tracking-wider">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200 border">
+                {orderItem.map((item) => (
+                  <>
+                    {item.item.map((ele) => (
+                      <tr key={ele._id}>
+                        <td className="flex px-6 py-2 whitespace-nowrap">
+                          <img
+                            src={ele.product.images}
+                            alt=""
+                            className="h-10 w-10 mr-10"
+                          />
+                          {ele.product.name}
+                        </td>
+                        <td className="px-6 py-2 whitespace-nowrap">
+                          {ele.quantity}
+                        </td>
+                        <td className="px-6 py-2 whitespace-nowrap">
+                          {ele.product.price}
+                        </td>
+                        <td className="px-6 py-2 whitespace-nowrap">
+                          {ele.price}
+                          <h1 hidden> {(total = total + ele.price)}</h1>
+                        </td>
+                        <td className="px-6 py-2 whitespace-nowrap">
+                          {
+                            <button
+                              // onClick={() => cancelCartItem(item.productId._id)}
+                              className="text-red-600 hover:text-red-900"
+                            >
+                              Cancel
+                            </button>
+                          }
+                        </td>
+                        <hr />
+                      </tr>
+                    ))}
+                  </>
+                ))}
+                <tr className="px-6 py-2 whitespace-nowra justify-between items-end">
+                  <td></td>
+                  <td></td>
+                  <td className="text-lg font-semibold">Total:</td>
+                  <td className="text-end text-lg font-semibold">{total}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="p-3 md:w-1/3">
+          <div className="border h-full w-full shadow-md ">
+            <div className="space-y-4 p-4">
+              <h1>
+                Total: <span className="float-right">{total}</span>
+              </h1>
+              <h1>
+                Charges:<span className="float-right">{charges}</span>
+              </h1>
+              <h1>
+                Discount: <span className="float-right">{discount}</span>
+              </h1>
+              <p hidden>{(grandTotal = total + charges - discount)}</p>
+              <h1>
+                Grand Total: <span className="float-right">{grandTotal}</span>
+              </h1>
+            </div>
+            <br />
+            <br />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Orders;
