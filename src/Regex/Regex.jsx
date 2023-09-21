de298b306463a0d2fdf3128ddaa37301351e9347
Mohamed Shareef <m.shareef.km@gmail.com>
2023-09-21 16:56:22 +0530

    Revert "fileChanges"
    This reverts commit 05a3c9b5ed7edd90d057a2cd55624b6aa5dfc95b.
    
    revert

--- src/Regex/Regex.jsx
+++
@@ -1,7 +0,0 @@
-export const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-export const PW_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{4,24}$/;
-export const NAME_REGEX = /^[A-Za-z]{4,20}$/;
-export const PHONE_REGEX = /^[0-9]{10}$/;
-export const POSTAL_CODE_REGEX = /^\d{6}$/;
-export const IMAGE_REGEX = /\.(jpg|jpeg|png|gif)$/i;
-export const PRICE_REGEX = /^(?!-)[1-9]\d*(\.\d+)?|0\.[1-9]\d*$/;
